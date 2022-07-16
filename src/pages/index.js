89c1e2f62191c1f55f18757662bcc3435f66dd3b
Muhamad Ardi Nur Insan <ardinurinsan03@gmail.com>
2022-07-16 12:01:20 +0700

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,68 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import { useAuth } from '@/hooks/auth'
+
+export default function Home() {
+    const { user } = useAuth({ middleware: 'guest' })
+
+    return (
+        <>
+            <Head>
+                <title>Laravel</title>
+            </Head>
+
+            <div className="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
+                <div className="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                    {user ? (
+                        <Link href="/dashboard">
+                            <a className="ml-4 text-sm text-gray-700 underline">
+                                Dashboard
+                            </a>
+                        </Link>
+                    ) : (
+                        <>
+                            <Link href="/login">
+                                <a className="text-sm text-gray-700 underline">
+                                    Login
+                                </a>
+                            </Link>
+
+                            <Link href="/register">
+                                <a className="ml-4 text-sm text-gray-700 underline">
+                                    Register
+                                </a>
+                            </Link>
+                        </>
+                    )}
+                </div>
+
+                <div className="max-w-6xl mx-auto sm:px-6 lg:px-8">
+                    <div className="flex justify-center pt-8 sm:justify-start sm:pt-0"></div>
+
+                    <div className="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
+                        <div className="grid grid-cols-1 md:grid-cols-2">
+                            <div className="p-6">
+                                <div className="flex items-center">
+                                    <svg
+                                        fill="none"
+                                        stroke="currentColor"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        viewBox="0 0 24 24"
+                                        className="w-8 h-8 text-gray-500">
+                                        <path d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
+                                    </svg>
+
+                                    <div className="ml-4 text-lg leading-7 font-semibold text-white">
+                                        Front End - Testing Pocobu
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
